c0e3f53db2af60f74caf4639551967e0c684e07d
ShanmugaSurya45 <shanmugasurya4145@gmail.com>
2026-09-17 15:34:19 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        unordered_map<char, char> mp = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        for (char c : s) {
+
+            // Opening bracket
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+            else {
+                if (st.empty() || st.top() != mp[c])
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
